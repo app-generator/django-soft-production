0992a7beb996c6762f81360884b6f983a114314f
App Generator <51070104+app-generator@users.noreply.github.com>
2022-05-23 17:45:03 +0300

    Replace ASSETS_ROOT with ASSETS_BASE
    for naming consistency

--- apps/context_processors.py
+++ apps/context_processors.py
@@ -3,7 +3,7 @@ from django.conf import settings
 def cfg_assets_root(request):
 
     if settings.DEBUG:
-        return { 'ASSETS_ROOT' : '/static/assets' }        
+        return { 'ASSETS_BASE' : '/static/assets' }        
     else:            
-        return { 'ASSETS_ROOT' : settings.ASSETS_BASE }
+        return { 'ASSETS_BASE' : settings.ASSETS_BASE }
 
